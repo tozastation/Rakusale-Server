ab35b0083a3d5b27ab6d2dddaae65842d7600827
tozastation <b1016031@fun.ac.jp>
2019-01-20 00:16:50 +0900

    [Add] Auto Migration

--- interfaces/server/handler/gorm_handler.go
+++ interfaces/server/handler/gorm_handler.go
@@ -1,17 +1,38 @@
 package handler
 
 import (
+	"fmt"
+	"github.com/2018-miraikeitai-org/Rakusale-Another-Server/domain/model"
 	"github.com/jinzhu/gorm"
+	"os"
 )
 
 var db *gorm.DB
 
 // OpenDBConnection is
 func OpenDBConnection() (*gorm.DB, error) {
-	db, err := gorm.Open("mysql", "root:@tcp(db:3306)/hoge")
+	dbType := os.Getenv("DB_TYPE")
+	connectionString := os.Getenv("CONNECTION_STRING")
+	db, err := gorm.Open(dbType, connectionString)
 	if err != nil {
 		return nil, err
 	}
+	// [Initialize] Database
+	db.AutoMigrate(&model.Vegetable{})
+	fmt.Println("[Init] Vegetable Model")
+	db.AutoMigrate(&model.Sell{})
+	fmt.Println("[Init] Sell Model")
+	db.AutoMigrate(&model.Buy{})
+	fmt.Println("[Init] Buy Model")
+	db.AutoMigrate(&model.SellList{})
+	fmt.Println("[Init] SellList Model")
+	db.AutoMigrate(&model.BuyList{})
+	fmt.Println("[Init] BuyList Model")
+	db.AutoMigrate(&model.Shop{})
+	fmt.Println("[Init] Shop Model")
+	db.AutoMigrate(&model.User{})
+	fmt.Println("[Init] User Model")
+
 	return db, nil
 }
 
