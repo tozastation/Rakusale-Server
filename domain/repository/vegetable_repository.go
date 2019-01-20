64767f850242088868e307fc7401d6b76c20dd91
tozastation <b1016031@fun.ac.jp>
2019-01-20 19:14:37 +0900

    [Fix] vegetable interface

--- domain/repository/vegetable_repository.go
+++ domain/repository/vegetable_repository.go
@@ -7,9 +7,10 @@ import (
 
 // VegetableRepository is Interface of Infrastructure Repository
 type VegetableRepository interface {
-	FindMyVegetables(ctx context.Context, userAccessToken string) ([]*protocol.Vegetable, error)
+	FindMyBoughtVegetables(ctx context.Context, token string) ([]*protocol.Vegetable, error)
+	FindMySoldVegetables(ctx context.Context, token string) ([]*protocol.Vegetable, error)
 	FindAllVegetables(ctx context.Context) ([]*protocol.Vegetable, error)
-	AddMyVegetable(ctx context.Context, userAccessToken string, user *protocol.Vegetable) error
-	UpdateMyVegetable(ctx context.Context, userAccessToken string, user *protocol.Vegetable) error
-	DeleteMyVegetable(ctx context.Context, userAccessToken string, vegetableID int64) error
+	AddMyVegetable(ctx context.Context, token string, v *protocol.Vegetable) error
+	UpdateMyVegetable(ctx context.Context, token string, vID int64, v *protocol.Vegetable) error
+	DeleteMyVegetable(ctx context.Context, token string, vID int64) error
 }
