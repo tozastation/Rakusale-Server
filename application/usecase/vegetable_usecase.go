2b99debb3ee5b663a397e776345262f1d0e773a8
tozastation <b1016031@fun.ac.jp>
2019-02-08 14:23:30 +0900

    [Add] Logger

--- application/usecase/vegetable_usecase.go
+++ application/usecase/vegetable_usecase.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/2018-miraikeitai-org/Rakusale-Another-Server/domain/repository"
 	pv "github.com/2018-miraikeitai-org/Rakusale-Another-Server/interfaces/server/rpc/vegetable"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -20,94 +21,120 @@ type VegetableUseCase interface {
 
 type vegetableUseCase struct {
 	repository.VegetableRepository
+	Logger *logrus.Logger
 }
 
 // NewVegetableUseCase is ...
-func NewVegetableUseCase(r repository.VegetableRepository) VegetableUseCase {
-	return &vegetableUseCase{r}
+func NewVegetableUseCase(r repository.VegetableRepository, l *logrus.Logger) VegetableUseCase {
+	return &vegetableUseCase{r, l}
 }
 
 func (u *vegetableUseCase) GetMyBoughtVegetables(ctx context.Context, p *pv.GetMyVegetablesRequest) (*pv.GetMyVegetablesResponse, error) {
+	u.Logger.Infoln("[START] GetMyBoughtVegetablesRPC is Called from Client")
 	res := pv.GetMyVegetablesResponse{}
 	token := p.GetToken()
+	u.Logger.Infoln("[INPUT] token=" + token)
 	vegetables, err := u.VegetableRepository.FindMyBoughtVegetables(ctx, token)
 	if err != nil {
+		u.Logger.Error("[EXECUTE FAILURE!] %s\n", err)
 		res.Status = http.StatusNoContent
 		return &res, nil
 	}
 	res.Vegetables = vegetables
 	res.Status = http.StatusOK
+	u.Logger.Infoln("[END] GetMyBoughtVegetablesRPC is Called from Client")
 	return &res, nil
 }
 
 func (u *vegetableUseCase) GetMySoldVegetables(ctx context.Context, p *pv.GetMyVegetablesRequest) (*pv.GetMyVegetablesResponse, error) {
+	u.Logger.Infoln("[START] GetMySoldVegetablesRPC is Called from Client")
 	res := pv.GetMyVegetablesResponse{}
 	token := p.GetToken()
+	u.Logger.Infoln("[INPUT] token=" + token)
 	vegetables, err := u.VegetableRepository.FindMySoldVegetables(ctx, token)
 	if err != nil {
+		u.Logger.Error("[EXECUTE FAILURE!] %s\n", err)
 		res.Status = http.StatusNoContent
 		return &res, nil
 	}
 	res.Vegetables = vegetables
 	res.Status = http.StatusOK
+	u.Logger.Infoln("[END] GetMySoldVegetablesRPC is Called from Client")
 	return &res, nil
 }
 
 func (u *vegetableUseCase) GetAllVegetables(ctx context.Context, p *pv.VegetablesEmpty) (*pv.GetAllVegetablesResponse, error) {
+	u.Logger.Infoln("[START] GetAllVegetablesRPC is Called from Client")
 	res := pv.GetAllVegetablesResponse{}
 	vegetables, err := u.VegetableRepository.FindAllVegetables(ctx)
 	if err != nil {
+		u.Logger.Error("[EXECUTE FAILURE!] %s\n", err)
 		res.Status = http.StatusNoContent
 		return &res, nil
 	}
 	res.Vegetables = vegetables
 	res.Status = http.StatusOK
+	u.Logger.Infoln("[END] GetAllVegetablesRPC is Called from Client")
 	return &res, nil
 }
 
 func (u *vegetableUseCase) GetSingleShopAllVegetables(ctx context.Context, p *pv.GetSingleShopAllVegetablesRequest) (*pv.GetSingleShopAllVegetablesResponse, error) {
+	u.Logger.Infoln("[START] GetSingleShopAllVegetablesRPC is Called from Client")
 	res := pv.GetSingleShopAllVegetablesResponse{}
 	sID := p.GetShopId()
+	u.Logger.Info("[INPUT] shopID=%s", sID)
 	vegetables, err := u.VegetableRepository.FindSingleShopAllVegetables(ctx, sID)
 	if err != nil {
+		u.Logger.Debug("[EXECUTE FAILURE!] %s\n", err)
 		res.Status = http.StatusNoContent
 		return &res, nil
 	}
 	res.Vegetables = vegetables
 	res.Status = http.StatusOK
+	u.Logger.Infoln("[END] GetSingleShopAllVegetablesRPC is Called from Client")
 	return &res, nil
 }
 
 func (u *vegetableUseCase) PostMyVegetable(ctx context.Context, p *pv.PostMyVegetableRequest) (*pv.PostMyVegetableResponse, error) {
+	u.Logger.Infoln("[START] PostMyVegetableRPC is Called from Client")
 	res := pv.PostMyVegetableResponse{}
 	token := p.GetToken()
+	u.Logger.Infoln("[INPUT] token=" + token)
 	err := u.VegetableRepository.AddMyVegetable(ctx, token, p)
 	if err != nil {
+		u.Logger.Error("[EXECUTE FAILURE!] %s\n", err)
 		res.Status = http.StatusBadRequest
 		return &res, nil
 	}
 	res.Status = http.StatusCreated
+	u.Logger.Infoln("[END] PostMyVegetableRPC is Called from Client")
 	return &res, nil
 }
 
 func (u *vegetableUseCase) PutMyVegetable(ctx context.Context, p *pv.PutMyVegetableRequest) (*pv.PutMyVegetableResponse, error) {
+	u.Logger.Infoln("[START] PutMyVegetableRPC is Called from Client")
 	res := pv.PutMyVegetableResponse{}
 	err := u.VegetableRepository.UpdateMyVegetable(ctx, p.GetToken(), p.GetVId(), p.GetVegetable())
 	if err != nil {
+		u.Logger.Error("[EXECUTE FAILURE!] %s\n", err)
 		res.Status = http.StatusBadRequest
 		return &res, nil
 	}
 	res.Status = http.StatusCreated
+	u.Logger.Infoln("[END] PutMyVegetableRPC is Called from Client")
 	return &res, nil
 }
 
 func (u *vegetableUseCase) DeleteMyVegetable(ctx context.Context, p *pv.DeleteMyVegetableRequest) (*pv.DeleteMyVegetableResponse, error) {
+	u.Logger.Infoln("[START] DeleteMyVegetableRPC is Called from Client")
 	res := pv.DeleteMyVegetableResponse{}
 	err := u.VegetableRepository.DeleteMyVegetable(ctx, p.GetToken(), p.GetVId())
 	if err != nil {
+		u.Logger.Error("[EXECUTE FAILURE!] %s\n", err)
 		res.Status = http.StatusBadRequest
 		return &res, nil
 	}
 	res.Status = http.StatusAccepted
+	u.Logger.Infoln("[END] DeleteMyVegetableRPC is Called from Client")
 	return &res, nil
 }
